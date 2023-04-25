df525def87f00cea4a103e273ac6e11823d3f47f
overlord64 <131316994+overlord64@users.noreply.github.com>
2023-04-25 14:21:40 -0700

    Add files via upload

---
+++ Proactive Remediation/LAPS/Detection.ps1
@@ -0,0 +1,20 @@
+Start-Transcript "c:\ProgramData\Microsoft\IntuneManagementExtension\Logs\lapsadmindetection.log"
+$username = "youradminuserhere"
+$userEnabled = $false
+$user = Get-LocalUser -Name $username -ErrorAction SilentlyContinue
+if ($user -and $user.Enabled) {
+	$userEnabled = $true
+      Write-Output ("User {0} present and enabled" -f $username)
+}
+If($userEnabled)
+{
+	Write-Output "User OK"
+	exit 0
+}
+else
+{
+	Write-Ouput "Error with user"
+	exit 1
+}
+   
+Stop-Transcript
---
+++ Proactive Remediation/LAPS/Remediate.ps1
@@ -0,0 +1,18 @@
+Start-Transcript "c:\ProgramData\Microsoft\IntuneManagementExtension\Logs\lapsadminremediation.log"
+Add-Type -AssemblyName 'System.Web'
+$username = "youradminuserhere"
+$user = Get-LocalUser -Name $username -ErrorAction SilentlyContinue
+$userParams = @{
+    Name = 'clientadmin'
+    Description = 'LAPS Client Admin'
+    Password = [System.Web.Security.Membership]::GeneratePassword(16, 0) | ConvertTo-SecureString -AsPlainText -Force
+}
+# create user with random password
+if(!$user)
+{
+	$user = New-LocalUser @userParams
+
+	# Add user to built-in administrators group
+	Add-LocalGroupMember -SID 'S-1-5-32-544' -Member $user
+}
+Stop-Transcript
